20960d11e674eff4d9a978e685ad6adc7a3cdc7c
Supakorn <supakorn0416@gmail.com>
2025-09-27 11:37:53 +0700

    feat: add mantine dropzone

--- src/app/MantineProvider.tsx
+++ src/app/MantineProvider.tsx
@@ -9,6 +9,7 @@ import { ModalsProvider } from "@mantine/modals";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import "@mantine/carousel/styles.css";
+import '@mantine/dropzone/styles.css';
 
 type Props = {
   children?: React.ReactNode;
