d9acffcee7ba35032fca54626b75d43d506fe552
Supakorn <supakorn0416@gmail.com>
2025-10-04 17:38:49 +0700

    feat: update @mantine/hooks to 8.3.2, add @mantine/spotlight, and implement SearchSpotlight component

--- src/app/MantineProvider.tsx
+++ src/app/MantineProvider.tsx
@@ -10,6 +10,7 @@ import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import "@mantine/carousel/styles.css";
 import "@mantine/dropzone/styles.css";
+import '@mantine/spotlight/styles.css';
 
 import theme from "./theme";
 
---
+++ src/components/Product/SearchSpotlight.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { useState } from "react";
+import { Spotlight, spotlight } from "@mantine/spotlight";
+import {
+  Center,
+  Group,
+  Text,
+  TextInput,
+  ActionIcon,
+  Box,
+  Image,
+  Rating,
+  Divider,
+} from "@mantine/core";
+import { IconSearch, IconClockHour3 } from "@tabler/icons-react";
+
+import { mockItems } from "./Recommend";
+
+const SearchSpotlight = () => {
+  const [query, setQuery] = useState("");
+
+  const items = mockItems
+    .filter((item: any) =>
+      item.name.toLowerCase().includes(query.toLowerCase().trim())
+    )
+    .map((item: any) => (
+      <Spotlight.Action key={item.id} onClick={() => console.log(item)}>
+        <Group wrap="nowrap" w="100%">
+          {item.image && (
+            <Center>
+              <Image
+                src={item.image}
+                alt={item.name}
+                h={80}
+                w={80}
+                maw="auto"
+                fit="cover"
+                radius="md"
+              />
+            </Center>
+          )}
+
+          <Box style={{ flex: 1 }}>
+            <Group justify="space-between" my={8}>
+              <Text fw={500}>{item.name}</Text>
+              <Text>฿{item.price}</Text>
+            </Group>
+            <Group>
+              <IconClockHour3 />
+              <Text>{item.cookTime}</Text>
+              <Divider size="sm" orientation="vertical" />
+              <Rating defaultValue={1} count={1} />
+              <Text>{item.rating}</Text>
+            </Group>
+          </Box>
+        </Group>
+      </Spotlight.Action>
+    ));
+
+  return (
+    <>
+      <Box
+        miw={160}
+        w="100%"
+        maw={400}
+        mx="auto"
+        p={16}
+        style={{
+          backdropFilter: "blur(2px)",
+          position: "relative",
+          zIndex: 1,
+          bottom: 50,
+          borderRadius: "80px",
+          boxShadow: "0 2px 2px rgba(0, 0, 0, 0.05)",
+        }}
+      >
+        <TextInput
+          radius="xl"
+          size="md"
+          placeholder="Search your favorite bakery"
+          rightSectionWidth={42}
+          style={{
+            backdropFilter: "blur(2px)",
+          }}
+          // styles={{
+          //   input: {
+          //     backgroundColor: "rgba(255, 255, 255, 0.8)",
+          //   },
+          // }}
+          // leftSection={<IconSearch size={18} stroke={1.5} />}
+          rightSection={
+            <ActionIcon
+              size={32}
+              radius="xl"
+              variant="filled"
+              onClick={spotlight.open}
+            >
+              <IconSearch size={18} stroke={1.5} />
+            </ActionIcon>
+          }
+        />
+      </Box>
+
+      <Spotlight.Root
+        query={query}
+        onQueryChange={setQuery}
+        scrollable
+        maxHeight={350}
+      >
+        <Spotlight.Search
+          placeholder="Search..."
+          leftSection={<IconSearch stroke={1.5} />}
+        />
+        <Spotlight.ActionsList>
+          {items.length > 0 ? (
+            items
+          ) : (
+            <Spotlight.Empty>Nothing found...</Spotlight.Empty>
+          )}
+        </Spotlight.ActionsList>
+      </Spotlight.Root>
+    </>
+  );
+};
+
+export default SearchSpotlight;
