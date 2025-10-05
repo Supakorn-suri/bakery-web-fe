3cff47d208fb6558faeb34ed46ccf87446e1b37e
Supakorn <supakorn0416@gmail.com>
2025-10-05 10:45:09 +0700

    feat: restructure and add member/baker routes

---
+++ src/app/(auth)/(member)/account/favorites/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FavoritePage = () => {
+  return (
+    <div>favoritePage</div>
+  )
+}
+
+export default FavoritePage
---
+++ src/app/(auth)/(member)/account/layout.tsx
@@ -0,0 +1,10 @@
+export default function AccountLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    
+    return <div>{children}</div>;
+  }
+  
+  
