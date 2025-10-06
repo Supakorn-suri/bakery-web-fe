f48b8edfbb69c7b905255c9bcc50378ae181cd98
Supakorn <supakorn0416@gmail.com>
2025-10-06 21:44:02 +0700

    feat: add HomeHeroSection component and style

---
+++ src/components/Home/HomeHeroSection.tsx
@@ -0,0 +1,91 @@
+import { Text, Image, Box } from "@mantine/core";
+
+import classes from "./HomeHeroSection.module.css";
+
+const HomeHeroSection = ({ id }: { id: string }) => {
+  return (
+    <Box
+      id={id}
+      mih="100dvh"
+      w="100%"
+      bg="#FBEAD0"
+      pos="relative"
+      style={{ overflow: "hidden" }}
+    >
+      <Image
+        top="-5%"
+        right="30%"
+        pos="absolute"
+        h={270}
+        w="auto"
+        fit="contain"
+        src="/plate_1.png"
+      />
+      <Image
+        top="30%"
+        right="-5%"
+        pos="absolute"
+        h={300}
+        w="auto"
+        fit="contain"
+        src="/plate_2.png"
+      />
+      <Image
+        top="50%"
+        left="3%"
+        pos="absolute"
+        h={450}
+        w="auto"
+        fit="contain"
+        src="/plate_3.png"
+      />
+      <Text className={classes.handwriting_text}>
+        <Text span style={{ display: "block" }} inherit>
+          Freshly baked happiness,
+        </Text>
+        <Text span style={{ display: "block" }} inherit>
+          just for you.
+        </Text>
+      </Text>
+      <Text
+        pos="absolute"
+        right="0"
+        top="30%"
+        style={{ transform: "rotate(-4deg)" }}
+        className={classes.handwriting_text}
+      >
+        <Text span style={{ display: "block" }} inherit>
+          Because life’s better
+        </Text>
+        <Text span style={{ display: "block" }} inherit>
+          with butter.
+        </Text>
+      </Text>
+      <Text
+        pos="absolute"
+        left="-1%"
+        bottom="27%"
+        style={{ transform: "rotate(4deg)" }}
+        className={classes.handwriting_text}
+      >
+        Every bite tells a story.
+      </Text>
+      <Text className={classes.main_text}>
+        <Text span style={{ display: "block" }} inherit>
+          Freshly
+        </Text>
+        <Text span style={{ display: "block" }} inherit>
+          baked
+        </Text>
+        <Text span style={{ display: "block" }} inherit>
+          happiness,
+        </Text>
+        <Text span style={{ display: "block" }} inherit>
+          just for you.
+        </Text>
+      </Text>
+    </Box>
+  );
+};
+
+export default HomeHeroSection;
